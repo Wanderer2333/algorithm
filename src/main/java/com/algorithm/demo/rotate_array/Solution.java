063b754468a9be5829121c5d68b8a1cccb67ffb4
JimWane <13754836986@163.com>
2019-11-28 20:25:51 +0800

    第一次正确答案提交。

--- src/main/java/com/algorithm/demo/rotate_array/Solution.java
+++ src/main/java/com/algorithm/demo/rotate_array/Solution.java
@@ -2,29 +2,20 @@ package com.algorithm.demo.rotate_array;
 
 class Solution {
     public void rotate(int[] nums, int k) {
-        /*if (k < nums.length) {
-            int[] temp = new int[nums.length];
-            for (int i = 0; i < nums.length; i++) {
-                if (i+k <= nums.length-1) {//往后挪
-                    temp[i+k] = nums[i];
-                }else{//往前挪
-                    temp[i+k - (nums.length-1)] = nums[i];
-                }
-                //temp[(i+k) % nums.length] = nums[i];
-            }
-            for (int i = 0; i <nums.length ; i++) {
-                nums[i] = temp[i];
-            }
-        }*/
+        k %=nums.length;
+        reverse(nums,0,nums.length -1);
+        reverse(nums,0,k -1);
+        reverse(nums,k,nums.length -1);
 
-        int[] temp = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            temp[(i+k) % nums.length] = nums[i];
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            nums[start] = nums[start] + nums[end];
+            nums[end] = nums[start] - nums[end];
+            nums[start] = nums[start] - nums[end];
+            start++;
+            end--;
         }
-
-        for (int i = 0; i <nums.length ; i++) {
-            nums[i] = temp[i];
-        }
-
     }
 }
